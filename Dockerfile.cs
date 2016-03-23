068d85d55e2accf6b9d561bfb833aa4c1021a93f
Evan Hazlett <ejhazlett@gmail.com>
2016-03-23 14:49:30 -0400

    update to 1.10.3-cs1
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- Dockerfile.cs
+++ Dockerfile.cs
@@ -2,7 +2,7 @@ FROM boot2docker/boot2docker
 MAINTAINER Evan Hazlett <ejhazlett@gmail.com>
 
 ENV SERIES 1.10
-ENV VERSION 1.10.2-cs1
+ENV VERSION 1.10.3-cs1
 
 RUN curl -fL -o $ROOTFS/usr/local/bin/docker https://packages.docker.com/$SERIES/builds/linux/amd64/docker-$VERSION && \
     chmod +x $ROOTFS/usr/local/bin/docker
